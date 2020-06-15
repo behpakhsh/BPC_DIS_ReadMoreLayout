1bdac71816845ab3fd98fa2d45104103f2ed8902
h.sohrabifard <hossein.sohrabi.94@gmail.com>
2020-06-15 12:05:00 +0430

    Avoid Leak of OnPreDrawListener

--- readmorelayout/src/main/java/bpc/dis/readmorelayout/ReadMoreLayoutOnPreDrawListener.java
+++ readmorelayout/src/main/java/bpc/dis/readmorelayout/ReadMoreLayoutOnPreDrawListener.java
@@ -15,18 +15,20 @@ public class ReadMoreLayoutOnPreDrawListener implements ViewTreeObserver.OnPreDr
     @Override
     public boolean onPreDraw() {
         ReadMoreLayout readMoreLayout = readMoreLayoutWeakReference.get();
-        if (!readMoreLayout.isInitTextView) {
-            return true;
-        }
-        readMoreLayout.textLines = readMoreLayout.textView.getLineCount();
-        boolean isExpandNeeded = readMoreLayout.textLines > readMoreLayout.expandLines;
-        readMoreLayout.isInitTextView = false;
-        if (isExpandNeeded) {
-            readMoreLayout.isShrink = true;
-            readMoreLayout.doAnimation(readMoreLayout.textLines, readMoreLayout.expandLines, readMoreLayout.WHAT_ANIMATION_END);
-        } else {
-            readMoreLayout.isShrink = false;
-            readMoreLayout.doNotExpand();
+        if (readMoreLayout != null) {
+            if (!readMoreLayout.isInitTextView) {
+                return true;
+            }
+            readMoreLayout.textLines = readMoreLayout.textView.getLineCount();
+            boolean isExpandNeeded = readMoreLayout.textLines > readMoreLayout.expandLines;
+            readMoreLayout.isInitTextView = false;
+            if (isExpandNeeded) {
+                readMoreLayout.isShrink = true;
+                readMoreLayout.doAnimation(readMoreLayout.textLines, readMoreLayout.expandLines, readMoreLayout.WHAT_ANIMATION_END);
+            } else {
+                readMoreLayout.isShrink = false;
+                readMoreLayout.doNotExpand();
+            }
         }
         return true;
     }
